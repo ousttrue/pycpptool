a7acba127401ef4c7425f18657437639d396cbcd
ousttrue <ousttrue@gmail.com>
2019-04-12 01:04:01 +0900

    typedef

--- pycpptool.py
+++ pycpptool.py
@@ -1,7 +1,7 @@
 import sys
 import platform
 import pathlib
-from typing import Set
+from typing import Dict, List, Optional, Any
 from clang import cindex
 
 
@@ -11,13 +11,44 @@ DEFAULT_CLANG_DLL = pathlib.Path(
     "C:/Program Files (x86)/LLVM/bin/libclang.dll")
 
 
+class Item_TypeDef:
+    def __init__(self, name: str, value: Any) -> None:
+        self.name = name
+        self.value = value
+
+    def __str__(self) -> str:
+        return f'{self.name}: {self.value}'
+
+
+class Item_Struct:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def __str__(self) -> str:
+        return f'struct {self.name}'
+
+
+class ParsedItem:
+    def __init__(self, key: int, filename: str) -> None:
+        self.key = key
+        self.filename = filename
+        self.content: Any = None
+
+    def __str__(self) -> str:
+        if self.content:
+            return str(self.content)
+        else:
+            return f'{self.key}'
+
+
 class Parser:
-    def __init__(self, dll: pathlib.Path=None) -> None:
+    def __init__(self, dll: pathlib.Path = None) -> None:
         if not dll:
             if platform.architecture()[0] == '32bit':
                 dll = DEFAULT_CLANG_DLL
         self.dll = dll
-        self.used: Set[int] = set()
+        self.item_map: Dict[int, ParsedItem] = {}
+        self.parsed_items: List[ParsedItem] = []
 
     def parse(self, path: pathlib.Path) -> None:
         if not path.exists():
@@ -30,30 +61,54 @@ class Parser:
                                        ['-x', 'c++'],
                                        options=cindex.TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD)
 
-        self.traverse(translation_unit.cursor)
+        # skip root translation_unit
+        for child in translation_unit.cursor.get_children():
+            self.traverse(child)
 
-    def traverse(self, cursor: cindex.Cursor, level: int = 0) -> None:
-        if cursor.hash in self.used:
+    def traverse(self, cursor: cindex.Cursor, level: int = 0) -> Optional[ParsedItem]:
+        used = self.item_map.get(cursor.hash)
+        if used:
             # already processed. skip
-            return
+            return used
 
-        self.used.add(cursor.hash)
+        if not cursor.location.file:
+            # skip
+            return None
 
-        # skip
-        if cursor.kind == cindex.CursorKind.TRANSLATION_UNIT:
-            filename = ''
-        else:
-            if not cursor.location.file:
-                # skip
-                return
-            filename = cursor.location.file.name
+        # new item
+        item = ParsedItem(cursor.hash, cursor.location.file)
+        self.item_map[cursor.hash] = item
+        self.parsed_items.append(item)
 
-            # process
-            print(f'{"  "*level}{cursor.kind}: {filename}')
+        # process
+        if cursor.kind == cindex.CursorKind.TYPEDEF_DECL:
+            tokens = [x.spelling for x in cursor.get_tokens()]
+            if len(tokens) == 3:
+                # typedef float FLOAT
+                item.content = Item_TypeDef(tokens[2], tokens[1])
+            elif len(tokens) < 3:
+                raise Exception(str(tokens))
+            else:
+                children = [x for x in cursor.get_children()]
+                count = len(children)
+                if count != 1:
+                    raise Exception(str(children))
+
+                item.content = Item_TypeDef(
+                    tokens[-1], self.item_map[children[0].hash])
+
+        elif cursor.kind == cindex.CursorKind.STRUCT_DECL:
+            item.content = Item_Struct(cursor.spelling)
 
         # children...
-        for child in cursor.get_children():
-            self.traverse(child, level+1)
+        if item.content:
+            print(f'{"  "*level}{item}')
+        else:
+            print(f'{"  "*level}{cursor.kind}')
+            for child in cursor.get_children():
+                self.traverse(child, level+1)
+
+        return item
 
 
 def main() -> None:
